97442696a6ff90bd7a453bd2785754b746810359
wkeylin <2530371305@qq.com>
2017-09-24 18:25:09 +0800

    结束

--- DepartmentInfo/app/scripts/index.js
+++ DepartmentInfo/app/scripts/index.js
@@ -1,3 +1,21 @@
+layui.use('layer', function () {
+    var layer = layui.layer;
+
+    $(".nav ul li").click(function () {
+
+        layer.open({
+            type: 1,
+            content: $('.container'),
+            area: ['3rem', '3.5rem'],
+            resize: false,
+            cancel: function(index, layero){
+                layer.close(index);
+                $(".container").hide();
+                return false; 
+              }
+        });
+    });
+});
 $(".nav ul li").mouseover(function () {
     $(this).addClass("animated pulse");
 }).mouseout(function () {
